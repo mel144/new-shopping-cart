b85e9c19545d2bb167000acff7fb9adc61c672dd
Molly Lansford <mollylansford2019@u.northwestern.edu>
2019-01-08 22:01:53 -0600

    Reading from ./static/data/products.json. Able to display product title and price.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <main>
+          <ProductTable products={this.props.products} />
+        </main>
       </div>
+    );
+  }
+}
+
+class ProductTable extends Component {
+  render() {
+    const rows = [];
+    let num_in_row = 4;
+    let temp_row = [];
+    let index = 0;
+    let num_rows = 0;
+
+    this.props.products.forEach((product) => {
+      if (index < num_in_row) {
+        temp_row.push(
+          product
+        );
+        index = index + 1;
+      } else {
+        console.log(temp_row);
+        rows.push(
+          <ProductRow products_in_row={temp_row} key={rows.length}/>
+        );
+        index = 0;
+        num_rows = num_rows + 1;
+        temp_row = [];
+      }
+    });
+
+    console.log(rows);
+
+    return (
+      <div className="shelf-container">
+        <table>
+          <tbody>{rows}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class ProductRow extends Component {
+  render() {
+    let row = [];
+
+    this.props.products_in_row.forEach((product) => {
+      row.push(
+        <ProductItem prod={product} key={product.sku}/>
+      );
+    });
+
+    return (
+      <tr>
+        {row}
+      </tr>
+    );
+  }
+}
+
+class ProductItem extends Component {
+  render() {
+    return (
+      <td className="shelf-item">
+        <div>{this.props.prod.title}</div>
+        <div>{this.props.prod.currencyFormat}{this.props.prod.price}</div>
+      </td>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import products from './static/data/products.json';
+
+ReactDOM.render(<App products={products.product_list} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
